f4ba0fbf4c80e4bda97d72ae905d09d6ebbc3489
Tomas Gic <gic.tomas@gmail.com>
2026-02-24 21:16:18 +0100

    feat: add a method for displaying user's balance

--- week-4/task1_oop_warmup/lib/bank.rb
+++ week-4/task1_oop_warmup/lib/bank.rb
@@ -17,6 +17,10 @@ class BankAccount
     @balance = balance 
   end
 
+  def display_balance
+    puts "Current balance is $#{'%.2f' % @balance}."
+  end
+  
   def deposit_amount(amount)
     @balance += amount
   end
