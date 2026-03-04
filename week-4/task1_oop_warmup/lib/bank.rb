b802a2179ed29e3ffb99a171b446dc49fb319909
Tomas Gic <gic.tomas@gmail.com>
2026-03-04 14:50:09 +0100

    fix: remove usage example from classes implementation file

--- week-4/task1_oop_warmup/lib/bank.rb
+++ week-4/task1_oop_warmup/lib/bank.rb
@@ -79,19 +79,3 @@ class CreditCard
   end
 end
 
-
-# owner = AccountOwner.new(first_name: "Tomas", last_name: "Gic")
-# account = BankAccount.new(owner: owner, balance: 2000)
-# credit_card = CreditCard.new(owner: owner, account: account, limit: 2500)
-
-# account.deposit_amount(3000)
-# account.display_balance
-# credit_card.display_balance
-
-# credit_card.charge(-540)
-# credit_card.display_balance
-
-# credit_card.make_payment(account: account, amount: 500)
-# credit_card.display_balance
-# account.display_balance
-
