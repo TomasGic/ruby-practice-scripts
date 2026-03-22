be765e733affb1be90f21b151c37dc9213b935cb
Tomas Gic <gic.tomas@gmail.com>
2026-03-22 20:18:21 +0100

    feat: implement string normalization and validation rules within the user class

--- week-5/random_people_cli/lib/random_people/user.rb
+++ week-5/random_people_cli/lib/random_people/user.rb
@@ -2,11 +2,13 @@ module RandomPeople
   class User
     attr_reader :first_name, :last_name, :full_name, :age, :group, :country
     def initialize(first_name:, last_name:, age:, country:)
-      @first_name = first_name
-      @last_name = last_name
-      @age = age
+      @first_name = normalize(first_name)
+      @last_name = normalize(last_name)
+      @age = Integer(age)
       @group = self.adult? ? "adult" : "minor"
-      @country = country
+      @country = normalize(country)
+
+      validate!
     end
 
     def full_name
@@ -15,6 +17,18 @@ module RandomPeople
 
     def adult?
       @age >= 18
+    end
+
+    private
+    def normalize(string)
+      String(string).strip.gsub(/\b\w/) { |char| char.upcase }
+    end
+
+    def validate!
+      raise "first_name required" if first_name.empty?
+      raise "last_name required" if last_name.empty?
+      raise "country required" if country.empty?
+      raise "age must be >= 0" if age < 0
     end
   end
 end
--- week-5/random_people_cli/spec/user_spec.rb
+++ week-5/random_people_cli/spec/user_spec.rb
@@ -7,14 +7,26 @@ RSpec.describe RandomPeople::User do
     described_class.new(
       first_name: "Tomas",
       last_name: "Gic",
-      age: 34
+      age: 34,
+      country: "Slovakia"
     )
   end
+  
   let(:user2) do 
     described_class.new(
       first_name: "Thomas",
       last_name: "Gic",
-      age: 17
+      age: 17,
+      country: "Slovakia"
+    )
+  end
+
+  let(:user3) do
+    described_class.new(
+      first_name: "mary-ann",
+      last_name: "gic ",
+      age: "34",
+      country: "slovakia"
     )
   end
   it "initalizes with full name attribute" do 
@@ -28,4 +40,21 @@ RSpec.describe RandomPeople::User do
   it "outputs minor category for users aged below 18" do
     expect(user2.group).to eq("minor")
   end
+
+  it "normalizes invalid arguments" do
+    expect(user3.full_name).to eq("Mary-Ann Gic")
+    expect(user3.country).to eq("Slovakia")
+    expect(user3.age).to eq(34)
+  end
+
+  it "raises error if user object tries to initialize with empty string arugment" do
+    expect {
+      described_class.new(
+        first_name: "",
+        last_name: "gic ",
+        age: "34",
+        country: "slovakia"
+      )
+  }.to raise_error(RuntimeError, "first_name required")
+  end    
 end
