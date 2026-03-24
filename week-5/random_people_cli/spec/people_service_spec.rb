a52948d6e9749a83806bc8c4af0b3049d1db39ce
Tomas Gic <gic.tomas@gmail.com>
2026-03-24 21:47:14 +0100

    test: add tests for the service orchestrator

--- week-5/random_people_cli/spec/people_service_spec.rb
+++ week-5/random_people_cli/spec/people_service_spec.rb
@@ -13,18 +13,38 @@ RSpec.describe RandomPeople::PeopleService do
   end
 
   describe "#execute" do
-    it "fetches raw response from the client and maps the response into array of user objects " do
-      num_users = raw_response["results"].size
-      fake_user = instance_double("RandomPeople::User")
-      
-      expect(client).to receive(:fetch).with(count:2).and_return(raw_response)
-      expect(mapper).to receive(:map_data).exactly(num_users).times.and_return(fake_user)
-      
-      results = service.execute
-      
-      expect(results).to be_an(Array)
-      expect(results.size).to eq(num_users)
-      expect(results.all? { |user| user == fake_user }).to eq(true)
+    context "when raw response contains data" do
+      it "fetches raw response from the client and maps the response into array of user objects " do
+        num_users = raw_response["results"].size
+        fake_user = instance_double("RandomPeople::User")
+        
+        expect(client).to receive(:fetch).with(count:2).and_return(raw_response)
+        expect(mapper).to receive(:map_data).exactly(num_users).times.and_return(fake_user)
+        
+        results = service.execute
+        
+        expect(results).to be_an(Array)
+        expect(results.size).to eq(num_users)
+        expect(results.all? { |user| user == fake_user }).to eq(true)
+      end
+    end
+    
+    context "when raw response is empty" do
+      it "fetches raw response from the client and returns an empty array without calling the mapper" do
+        empty_response = { "results" => [] }
+        expect(client).to receive(:fetch).and_return(empty_response)
+        expect(mapper).not_to receive(:map_data)
+        expect(service.execute).to eq([])
+      end
+    end
+
+    context "when raw response is broken and/or results key is missing" do
+      it "fetches the broken response from the client and returns an empty array" do
+        broken_response = { "error" => "oops"}
+        expect(client).to receive(:fetch).and_return(broken_response)
+        expect(mapper).not_to receive(:map_data)
+        expect(service.execute).to eq([])
+      end
     end
   end
 end
