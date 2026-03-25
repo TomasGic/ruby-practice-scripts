b00e92750ef0317f6ea4cb447dfc036fdcd19ffa
Tomas Gic <gic.tomas@gmail.com>
2026-03-26 00:20:12 +0100

    feat: implement the real http adapter class

--- week-5/random_people_cli/lib/random_people/http/net_http_adapter.rb
+++ week-5/random_people_cli/lib/random_people/http/net_http_adapter.rb
@@ -1,13 +1,34 @@
-class FakeHttp
-  attr_reader :seen_urls
-  def initialize(status:, body:)
-    @status = status
-    @body = body
-    @seen_urls = []
-  end
+module RandomPeople
+  module HTTP
+    class FakeHttp
+      attr_reader :seen_urls
+      def initialize(status:, body:)
+        @status = status
+        @body = body
+        @seen_urls = []
+      end
 
-  def get(url, headers: {})
-    @seen_urls << url
-    RandomPeople::HTTP::Response.new(status: @status, body: @body)
+      def get(url, headers: {})
+        @seen_urls << url
+        RandomPeople::HTTP::Response.new(status: @status, body: @body)
+      end
+    end
+
+    class RealHttp
+      def get(url, headers: {})
+        uri = URI(url)
+        req = Net::HTTP::Get.new(uri)
+        headers.each { |k, v| req[k] = v }
+
+        res = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == "https") do |http|
+          http.request(req)
+        end
+        RandomPeople::HTTP::Response.new(status: res.code.to_i, body: res.body.to_s)
+      end
+    end
+      
   end
 end
+    
+  
+
--- week-5/random_people_cli/spec/random_user_client_spec.rb
+++ week-5/random_people_cli/spec/random_user_client_spec.rb
@@ -1,10 +1,10 @@
 require "json"
 # rspec spec/random_user_client_spec.rb
 RSpec.describe RandomPeople::Clients::RandomUserClient do
-  it "correctly fetches json payload from api using fake http client" do
+  it "correctly fetches json payload using fake http client" do
     fixture_path = File.expand_path("fixtures/random_people.json", __dir__)
     json = File.read(fixture_path)
-    http = FakeHttp.new(status: 200, body: json)
+    http = RandomPeople::HTTP::FakeHttp.new(status: 200, body: json)
     client = described_class.new(http: http)
     payload = client.fetch(count: 1)
     
@@ -15,14 +15,14 @@ RSpec.describe RandomPeople::Clients::RandomUserClient do
   end
 
   it "raises api error when response code is 500 (server error)" do
-    http = FakeHttp.new(status: 500, body: "oops")
+    http = RandomPeople::HTTP::FakeHttp.new(status: 500, body: "oops")
     client = described_class.new(http: http)
 
     expect { client.fetch(count: 1) }.to raise_error("api error")
   end
 
   it "raises json parser error when json is invalid" do
-    http = FakeHttp.new(status: 200, body: "not json}")
+    http = RandomPeople::HTTP::FakeHttp.new(status: 200, body: "not json}")
     client = described_class.new(http: http)
 
     expect { client.fetch(count: 1) }.to raise_error(JSON::ParserError)
