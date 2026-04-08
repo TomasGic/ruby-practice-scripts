012ff66cc0910fa89a02338fe6645c6197a5db42
Tomas Gic <gic.tomas@gmail.com>
2026-04-08 16:14:50 +0200

    feat: add bin/demo executable file to run the program script from

--- week-6/task1_shipping_calculator/lib/shipping/calculators/base.rb
+++ week-6/task1_shipping_calculator/lib/shipping/calculators/base.rb
@@ -13,21 +13,23 @@ module Shipping
       end
     end
 
+    def currency
+      "$"
+    end
+
     def calculate(package)
       
       log validate_package(package)
       
       base_rate = compute_base_rate(package)
-      log "Base rate: #{base_rate}"
+      log "Base rate: #{currency}#{base_rate}"
       
       surcharge = apply_surcharges(package)
-      log "Surcharge: #{surcharge}" if surcharge > 0.00
-      log "No surcharges applied"
+      log(surcharge > 0 ? "Surcharge: #{currency}#{surcharge}" : "No surcharges applied")
       
       subtotal = base_rate + surcharge
       discount = apply_discount(package, subtotal)
-      log "Discount: #{discount}" if discount > 0.00
-      log "No discounts applied"
+      log(discount > 0 ? "Discount: #{currency}#{discount}" : "No discount applied")
       
       total = (subtotal - discount).round(2)
       build_result(base_rate: base_rate, surcharge: surcharge, discount: discount, total: total)
@@ -37,7 +39,7 @@ module Shipping
     private 
 
     def validate_package(package)
-      "Validating package: #{package.weight}, zone: #{package.zone}"
+      "Validating package: #{package.weight}kg, zone: #{package.zone}"
     end
 
     def build_result(base_rate:, surcharge:, discount:, total:)
--- week-6/task1_shipping_calculator/lib/shipping/package.rb
+++ week-6/task1_shipping_calculator/lib/shipping/package.rb
@@ -4,7 +4,7 @@ module Shipping
   class Package
     attr_reader :weight, :length, :width, :height, :zone
     
-    def initialize(weight:, length:, width:, height:, zone:)
+    def initialize(weight: nil, length: nil, width: nil, height: nil, zone: nil)
       begin
         @weight, @length, @width, @height = [weight, length, width, height].map do |val|
           Float(val)
@@ -13,9 +13,6 @@ module Shipping
         raise InvalidPackageError, "Weight and dimensions must be numeric"
       end
 
-      if zone.nil? || zone.to_s.strip.empty?
-        raise InvalidPackageError, "Zone is required!"
-      end
       @zone = zone
       
       validate!
@@ -24,6 +21,9 @@ module Shipping
     def validate!
       if [@weight, @length, @width, @height].any? { |attr| attr <= 0 }
         raise InvalidPackageError, "Weight and/or dimensions cannot be negative or zero!"
+      end
+      if @zone.nil? || @zone.to_s.strip.empty?
+        raise InvalidPackageError, "Zone is required"
       end
     end
   end
