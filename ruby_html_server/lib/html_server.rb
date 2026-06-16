0efc644f6e55861213ec20274f88b7ea3c93d14a
Tomas Gic <gic.tomas@gmail.com>
2026-06-16 17:07:19 +0200

    chore: add project folder structure

---
+++ ruby_html_server/lib/html_server.rb
@@ -0,0 +1,8 @@
+require_relative "html_builder"
+require_relative "style_builder"
+require_relative "page_builder"
+require_relative "router"
+
+require_relative "pages/home_page"
+require_relative "pages/about_page"
+require_relative "pages/contact_page"
