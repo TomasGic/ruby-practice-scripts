1b74dffd806059f253e9f9e3873b04d184ab0406
Tomas Gic <gic.tomas@gmail.com>
2026-06-18 16:20:49 +0200

    feat: implement StyleBuilder class and methods that generate css styles

---
+++ ruby_html_server/lib/style_builder.rb
@@ -0,0 +1,33 @@
+class StyleBuilder
+  def initialize
+    @rules = []
+  end
+
+  def self.build(&block)
+    builder = new
+    block.call(builder)
+    builder.to_css
+  end
+
+  def rule(selector, **properties)
+    properties = properties.map do |key, value|
+      key = key.to_s.gsub("_", "-")
+      " #{key}: #{value};"
+    end.join("\n")
+    
+    @rules << "#{selector} {\n#{properties}\n}"
+
+  end
+
+  def media(condition, &block)
+    builder = StyleBuilder.new
+    block.call(builder)
+    inner_css_string = builder.to_css.split("\n").map { |line| "  #{line}" }.join("\n")
+    
+    @rules << "@media (#{condition}) {\n#{inner_css_string}\n}"
+  end
+
+  def to_css
+    @rules.join("\n")
+  end
+end
