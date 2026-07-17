0890f0e2570a1099a9d519e79cee468d441fe610
Tomas Gic <gic.tomas@gmail.com>
2026-07-17 18:38:43 +0200

    feat: implement base validator class

---
+++ week-6/task3_payment_pipeline/lib/payment_pipeline/validators/base_validator.rb
@@ -0,0 +1,41 @@
+module PaymentPipeline
+ 
+  class BaseValidator
+    attr_reader :next_handler
+   
+    def initialize(next_handler: nil)
+      @next_handler = next_handler
+    end
+
+
+    def validate(request)
+      if can_validate?(request)
+        result = perform_validation(request)
+        #if validation fails, result[:valid] is false and we stop the validation chain
+        return result unless result[:valid]
+      end
+
+
+      #we pass the validation on to the next handler if there is one
+      if @next_handler
+        @next_handler.validate(request)
+      end
+
+
+
+
+      #if earlier validation was successful or a given validator is unable to validate the request the method returns the hash below
+      { valid: true }
+    end
+
+
+    def can_validate?(request)
+      raise NotImplementedError, "{self.cass} must implement #can_validate?"
+    end
+
+
+    def perform_validation(request)
+      raise NotImplementedError, "{self.class} must implement #perform_validation"
+    end
+  end
+end
---
+++ week-6/task3_payment_pipeline/spec/validators/base_validator_spec.rb
@@ -0,0 +1,76 @@
+RSpec.describe PaymentPipeline::BaseValidator do
+  let(:passing_test_request) { {should_run: true, should_pass: true } }
+  let(:failing_test_request) { {should_run: true, should_pass: false } }
+  let(:dummy_validator_class) do
+    Class.new(described_class) do
+      def can_validate?(request)
+        request[:should_run]
+      end
+
+
+      def perform_validation(request)
+        if request[:should_pass]
+          return { valid: true }  
+        else
+          return { valid: false, error: "Validation failed", validator: self.class }
+        end
+      end
+    end
+  end
+  it "initializes with next handler as nil if no next handler passed as argument" do
+    validator = described_class.new
+    expect(validator.next_handler).to be_nil
+  end
+
+
+  describe "validate" do
+    context "when there is no next handler" do
+      before do
+        @validator = dummy_validator_class.new
+      end
+      it "returns success when validation passes" do
+        result = @validator.validate(passing_test_request)
+        expect(result[:valid]).to be true
+      end
+
+
+      it "returns failure when validation fails" do
+        result = @validator.validate(failing_test_request)
+        expect(result[:valid]).to be false
+        expect(result[:error]).to eq("Validation failed")
+        expect(result[:validator]).to eq(dummy_validator_class)
+      end
+
+
+      it "skips validation and returns success if validator cannot validate request" do
+        request = { should_run: false }
+        result = @validator.validate(request)
+        expect(result[:valid]).to be true
+      end
+
+
+
+
+    end
+
+
+    context "when there is next handler in the validation chain" do
+      let(:next_validator) { dummy_validator_class.new }
+      let(:chain) { dummy_validator_class.new(next_handler: next_validator) }
+      it "passes the request on to the next handler if the first handler validates the request" do
+        expect(next_validator).to receive(:validate).and_call_original
+        result = chain.validate(passing_test_request)
+        expect(result[:valid]).to be true
+      end
+
+
+      it "stops the chain immediately and returns failure if the first validation fails" do
+       
+        expect(next_validator).not_to receive(:validate)
+        result = chain.validate(failing_test_request)
+        expect(result[:valid]).to be false
+      end
+    end
+  end
+end
+
